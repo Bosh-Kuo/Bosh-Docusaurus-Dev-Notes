721af7cf3beefef455f621ade39aba5d26bbfc0d
Bosh-Kuo <boshkuo.dev@gmail.com>
2024-06-09 16:11:40 +0800

    [refactor] MDX typo & re-index & 調整 script

--- scripts/reindex_files_dirs.sh
+++ scripts/reindex_files_dirs.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # description: 
-#   This script increments the numbering of directories within the specified range.
-#   Each directory within the range will have its number increased by 1.
-#   The directory names should follow the pattern NN-xxx, where NN is a two-digit number.
+#   This script increments the numbering of directories and .md files within the specified range.
+#   Each directory and file within the range will have its number increased by 1.
+#   The names should follow the pattern NN-xxx, where NN is a two-digit number.
 # Usage: 
 #   ./rename_folders.sh <directory> <start_number> <end_number>
 # ex: 
 #   ./rename_folders.sh ../docs 01 05
 #   This command will rename directories in the docs folder from 01-xxx to 02-xxx, 02-xxx to 03-xxx, and so on up to 05-xxx to 06-xxx.
+#   ./rename_items.sh ../docs/01-Docusaurus 01 05
+#   This command will rename files in the ../docs/01-Docusaurus folder such as 01-xxx.md to 02-xxx.md, 02-xxx.md to 03-xxx.md, and so on up to 05-xxx.md to 06-xxx.md.
 
 # 檢查是否提供了正確的參數
 if [ "$#" -ne 3 ]; then
@@ -40,11 +42,21 @@ for (( i=END_NUM; i>=START_NUM; i-- )); do
     CURRENT_NUM=$(printf "%02d" $i)
     NEW_NUM=$(printf "%02d" $((i+1)))
     
-    for dir in ${CURRENT_NUM}-*; do
-        if [ -d "$dir" ]; then
-            NEW_DIR="${NEW_NUM}-${dir#*-}"
-            echo "Renaming $dir to $NEW_DIR"
-            mv "$dir" "$NEW_DIR"
+    # 重命名目錄
+    for item in ${CURRENT_NUM}-*; do
+        if [ -d "$item" ]; then
+            NEW_ITEM="${NEW_NUM}-${item#*-}"
+            echo "Renaming directory $item to $NEW_ITEM"
+            mv "$item" "$NEW_ITEM"
+        fi
+    done
+
+    # 重命名文件
+    for item in ${CURRENT_NUM}-*.md; do
+        if [ -f "$item" ]; then
+            NEW_ITEM="${NEW_NUM}-${item#*-}"
+            echo "Renaming file $item to $NEW_ITEM"
+            mv "$item" "$NEW_ITEM"
         fi
     done
 done
