98d23a5758977e4c2a3856865c95fa367bc766b7
Bosh-Kuo <boshkuo.dev@gmail.com>
2024-09-30 22:41:14 +0800

    refactor: 調整 iterm 文章路徑

--- config/redirects.ts
+++ config/redirects.ts
@@ -3,6 +3,10 @@ const redirects = [
     to: "/docs/React/TS-Jest-RTL-MSW-Setup",
     from: "/docs/Testing/TS-Jest-RTL-MSW-Setup",
   },
+  {
+    to: "/docs/DevTools/Terminal/iterm-shortcut-keys",
+    from: "/docs/DevTools/iterm-shortcut-keys",
+  },
 ];
 
 export default redirects;
--- docs/11-DevTools/04-Terminal/01-iterm-shortcut-key.md
+++ docs/11-DevTools/04-Terminal/01-iterm-shortcut-key.md
@@ -1,6 +1,6 @@
 ---
 title: iTerm2 技巧大公開：提高效率的快捷鍵
-sidebar_label: "[Terminal] iTerm2 常用快捷鍵大全"
+sidebar_label: "[iTerm2] 常用快捷鍵大全"
 description: 本篇文章介紹 iTerm2 中常用的快捷鍵，以及許多好用但一般人很少用的快捷鍵
 last_update:
   date: 2023-04-22
