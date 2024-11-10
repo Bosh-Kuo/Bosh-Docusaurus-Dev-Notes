f2ce71c07e2f7645d8223ecfb08ac72ad72dff31
Bosh-Kuo <boshkuo.dev@gmail.com>
2024-11-11 01:05:15 +0800

    feat: 調整 Project 頁面樣式

--- src/components/RepoCard/index.tsx
+++ src/components/RepoCard/index.tsx
@@ -11,36 +11,37 @@ const RepoCard = ({ repo }) => {
   const { colors, hasError } = useColors();
 
   return (
-    <div className={styles.repo_card}>
-      <div className={styles.repo_card__header}>
-        <svg
-          viewBox='0 0 16 16'
-          version='1.1'
-          width='16'
-          height='16'
-          aria-hidden='true'
+    <a
+      target='_blank'
+      href={repo.html_url}
+      rel='noopener noreferrer'
+      style={{ textDecoration: "none" }}
+    >
+      <div className={styles.repo_card}>
+        <div className={styles.repo_card__header}>
+          <svg
+            viewBox='0 0 16 16'
+            version='1.1'
+            width='16'
+            height='16'
+            aria-hidden='true'
+          >
+            <path
+              fillRule='evenodd'
+              d='M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z'
+            ></path>
+          </svg>
+          <span>{repo.name}</span>
+        </div>
+        <div
+          className={styles.repo_card__description}
+          style={{ fontStyle: !repo.description ? "italic" : "normal" }}
         >
-          <path
-            fillRule='evenodd'
-            d='M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z'
-          ></path>
-        </svg>
-        <span>
-          <a target='_blank' href={repo.html_url}>
-            {repo.name}
-          </a>
-        </span>
-      </div>
-      <div
-        className={styles.repo_card__description}
-        style={{ fontStyle: !repo.description ? "italic" : "normal" }}
-      >
-        {repo.description}
-      </div>
-      <div className={styles.repo_card__metadata}>
-        {repo.language && (
-          <div className={styles.repo_card__metadata__language}>
-            <a href={repo.language in colors ? colors[repo.language].url : ""}>
+          {repo.description}
+        </div>
+        <div className={styles.repo_card__metadata}>
+          {repo.language && (
+            <div className={styles.repo_card__metadata__language}>
               <span
                 style={{
                   backgroundColor:
@@ -48,12 +49,10 @@ const RepoCard = ({ repo }) => {
                 }}
               ></span>
               <span>{repo.language}</span>
-            </a>
-          </div>
-        )}
+            </div>
+          )}
 
-        <div className={styles.repo_card__metadata__stargazers}>
-          <a href={repo.html_url}>
+          <div className={styles.repo_card__metadata__stargazers}>
             <svg
               aria-label='stars'
               viewBox='0 0 16 16'
@@ -68,11 +67,9 @@ const RepoCard = ({ repo }) => {
               ></path>
             </svg>
             &nbsp; <span>{repo.stargazers_count}</span>
-          </a>
-        </div>
+          </div>
 
-        <div className={styles.repo_card__metadata__forks}>
-          <a href={repo.html_url}>
+          <div className={styles.repo_card__metadata__forks}>
             <svg
               aria-label='fork'
               viewBox='0 0 16 16'
@@ -87,10 +84,10 @@ const RepoCard = ({ repo }) => {
               ></path>
             </svg>
             &nbsp; <span>{repo.forks}</span>
-          </a>
+          </div>
         </div>
       </div>
-    </div>
+    </a>
   );
 };
 
